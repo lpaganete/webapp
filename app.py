227f127827728b66ea153be50b5041d0e96bbe09
Luís Paulo Aganete Reis <40781159+lpaganete@users.noreply.github.com>
2023-04-14 16:06:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">Patica 2 Trabalho final</p></h1>'
+    return '<h1><p style="color:' + os.environ.get('APP_COLOR', 'black') + '">PTestando Webhook</p></h1>'
 
 @app.route('/api')
 def hello():
